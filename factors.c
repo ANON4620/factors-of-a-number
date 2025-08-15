1beff1fc04dc0e414ac55924f08b3412effd3391
ANON4620 <swarnadeep.click@gmail.com>
2025-08-15 17:20:10 -0300

    Implemented dynamic allocation to store small factors and larger factors in separately, then merge them in ascending order; suggested by reddit user whoyfear; also added status code return by the function for detecting success or failure.

--- factors.c
+++ factors.c
@@ -7,24 +7,53 @@ void printArray(int A[], int n) {
     printf("\n");
 }
 
-void findFactors(int n, int **factors, int *returnSize) {
-    int temp[n];
-    int idx = 0, last_idx = n - 1;
-    *returnSize = 0;
+int findFactors(int n, int **factors, int *returnSize) {
+
+    int *small = (int *) malloc(sizeof(int));
+    if(small == NULL) {
+        return 0;
+    }
+    int *big = (int *) malloc(sizeof(int));
+    if(big == NULL) {
+        return 0;
+    }
+
+    int sidx = 0, bidx = 0, slen = 1, blen = 1;
 
     int step;
-    if(n % 2 == 0)
+    if(n % 2 == 0) {
         step = 1;           // since factors of an even number can be either even or odd
-    else
+    }
+    else {
         step = 2;           // since all factors of an odd number are odd
+    }
 
+    *returnSize = 0;
     int i;
     for(i = 1; i * i < n; i += step) {
         if(n % i == 0) {
-            temp[idx] = i;
-            idx++;
-            temp[last_idx] = n / i;
-            last_idx--;
+            small[sidx] = i;
+            sidx++;
+            if(sidx == slen) {
+                slen *= 2;
+                int *newSmall = (int *) realloc(small, slen * sizeof(int));
+                if(newSmall == NULL) {
+                    free(small);
+                    return 0;
+                }
+                small = newSmall;
+            }
+            big[bidx] = n / i;
+            bidx++;
+            if(bidx == blen) {
+                blen *= 2;
+                int *newBig = (int *) realloc(big, blen * sizeof(int));
+                if(newBig == NULL) {
+                    free(big);
+                    return 0;
+                }
+                big = newBig;
+            }
             *returnSize += 2;
         }
     }
@@ -32,19 +61,24 @@ void findFactors(int n, int **factors, int *returnSize) {
     if(n % i == 0) {
         // this inserts a single factor M into the array such that M * M = n
         if(i == (n / i)) {
-            temp[idx] = i;
-            idx++;
+            small[sidx] = i;
+            sidx++;
             *returnSize += 1;
         }
     }
 
     *factors = (int *) malloc((*returnSize) * sizeof(int));
-    for(int i = 0; i < idx; i++) {
-        (*factors)[i] = temp[i];
+    for(int i = 0; i < sidx; i++) {
+        (*factors)[i] = small[i];
     }
-    for(int i = idx; i < (*returnSize); i++) {
-        (*factors)[i] = temp[++last_idx];
+    for(int i = sidx; i < (*returnSize); i++) {
+        (*factors)[i] = big[--bidx];
     }
+
+    free(small);
+    free(big);
+
+    return 1;
 }
 
 int main() {
@@ -53,7 +87,13 @@ int main() {
     int size;
     printf("Enter the number: ");
     scanf("%d", &n);
-    findFactors(n, &factors, &size);
-    printArray(factors, size);
+    int status = findFactors(n, &factors, &size);
+    if(status == 1) {
+        printArray(factors, size);
+        free(factors);
+    }
+    else {
+        printf("factors() function failed!");
+    }
     return 0;
 }
